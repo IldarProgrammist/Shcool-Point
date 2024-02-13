7523034a19eb62e3796150a197ce2e397dc34423
ildar <UrazbakhtinIV@gmail.com>
2024-02-13 15:40:20 +0300

    Change ConsoleApp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,22 +1,25 @@
-﻿using CrmDBLab.Models;
-using static System.Console;
+﻿using static System.Console;
+using CrmDBLab.Models;
 
-CityModel cityModel = new();
-
-
-CityModel city = new CityModel();
-
-//city.CityAdd(7,"бла бла бла");
-
-city.CityDelete(7);
-
-//city.CityUpdate(6,"Ижевск");
-
-
-foreach (var cityList in city.GetCityList())
+namespace ConsoleApp
 {
-    WriteLine(cityList.CityName);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CityModel city = new CityModel();
+
+            //city.CityAdd(7,"бла бла бла");
+
+            //city.CityDelete(7);
+
+            //city.CityUpdate(6,"Ижевск");
+
+
+            foreach (var cityList in city.GetCityList())
+            {
+                WriteLine(cityList.CityName);
+            }
+        }
+    }
 }
-
-
-
