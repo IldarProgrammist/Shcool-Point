987c2b4943b9e31b96463c6068170c35e04b3b29
ildar <UrazbakhtinIV@gmail.com>
2024-02-21 23:09:55 +0300

    add method GetAddress

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,33 +11,41 @@ namespace ConsoleApp
                 AreaModel area = new();
                 AddressModel addressModel = new();
                 CityModel cityModel = new();
-
+                AreaModel areaModel = new();
+                StreetModel streetModel = new();
 
             //cityModel.CityAdd("бла бла бла");
             //cityModel.CityUpdate(6,"Новосибирск");
             //cityModel.CityDelete(4);
 
-             AreaModel areaModel = new();
+
             //areaModel.UpdateArea(1,"Арбат");
             //areaModel.AddArea("Бла бла бла");
             //areaModel.DeleteArea(2);
 
-           /* foreach (var item in cityModel.GetCityList())
-            {
-                WriteLine($"Город:{item.CityName}");
-            }*/
+            /* foreach (var item in cityModel.GetCityList())
+             {
+                 WriteLine($"Город:{item.CityName}");
+             }*/
 
-           /* foreach (var item in areaModel.GetListAll())
-            {
-                WriteLine($"Район:{item.AreaName}");
-            }*/
-        
-        
-            StreetModel streetModel = new();
+            /* foreach (var item in areaModel.GetListAll())
+             {
+                 WriteLine($"Район:{item.AreaName}");
+             }*/
 
-            foreach (var item in streetModel.GetStreetList())
+            /* foreach (var item in streetModel.GetStreetList())
+             {
+                 WriteLine(item.StreetName);
+             }*/
+
+            foreach (var item in addressModel.GetAddress())
             {
-                WriteLine(item.StreetName);
+                WriteLine($"Город:{item.City.CityName} " +
+                          $"Район: {item.Area.AreaName} " +
+                          $"Улица: {item.Street.StreetName} " +
+                          $"Дом: {item.Building} " +
+                          $"Корпус: {item.Construction}"
+                          );
             }
 
             //streetModel.StreetAdd("Ленина");
--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c35ea6d7083230ada06a28a4b879679db587ef2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c904d20a7a69e2b759b2d144367be8b858784d5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
